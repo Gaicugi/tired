b0e8bf577d715df3ca01d6eb656a897cebf800f6
Gaicugi <95116161+Gaicugi@users.noreply.github.com>
2026-03-29 23:54:18 +0300

    Create stunipscan.py

---
+++ stunipscan.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Jose Luis Verdeguer'
+__email__ = "pepeluxx@gmail.com"
+
+from modules.stunipscan import StunIpscan
+from lib.params import get_stunipscan_args
+
+
+def main():
+    ip, host, rport, proto, verbose, user, pwd, ipdst = get_stunipscan_args()
+
+    s = StunIpscan()
+    s.ip = ip
+    s.host = host
+    s.rport = rport
+    s.proto = proto
+    s.verbose = verbose
+    s.user = user
+    s.pwd = pwd
+    s.ipdst = ipdst
+
+    s.start()
+
+
+if __name__ == '__main__':
+    main()
