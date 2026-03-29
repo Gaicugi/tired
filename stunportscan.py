7ee7ae1c2d0df4633e2371fda3f86d6d3c323820
Gaicugi <95116161+Gaicugi@users.noreply.github.com>
2026-03-29 23:56:01 +0300

    Create stunportscan.py

---
+++ stunportscan.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Jose Luis Verdeguer'
+__email__ = "pepeluxx@gmail.com"
+
+from modules.stunportscan import StunPortscan
+from lib.params import get_stunportscan_args
+
+
+def main():
+    ip, host, rport, proto, verbose, user, pwd, ipdst, ports, fp = get_stunportscan_args()
+
+    s = StunPortscan()
+    s.ip = ip
+    s.host = host
+    s.rport = rport
+    s.proto = proto
+    s.verbose = verbose
+    s.user = user
+    s.pwd = pwd
+    s.ipdst = ipdst
+    s.ports = ports
+    s.fp = fp
+
+    s.start()
+
+
+if __name__ == '__main__':
+    main()
