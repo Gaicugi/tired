59272ab0c0bdd4f3305e6a3483a49388e0a35493
Gaicugi <95116161+Gaicugi@users.noreply.github.com>
2026-03-29 23:56:52 +0300

    Create stunscan.py

---
+++ stunscan.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Jose Luis Verdeguer'
+__email__ = "pepeluxx@gmail.com"
+
+from modules.stunscan import StunScan
+from lib.params import get_stunscan_args
+
+
+def main():
+    ip, host, rport, proto, threads, verbose, nocolor, file, ofile, random = get_stunscan_args()
+
+    s = StunScan()
+    s.ip = ip
+    s.host = host
+    s.rport = rport
+    s.proto = proto
+    s.threads = threads
+    s.verbose = verbose
+    s.nocolor = nocolor
+    s.file = file
+    s.ofile = ofile
+    s.random = random
+
+    s.start()
+
+
+if __name__ == '__main__':
+    main()
