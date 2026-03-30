e9967f2fcc46666a52d382d36654bde20b1b4cbc
Gaicugi <95116161+Gaicugi@users.noreply.github.com>
2026-03-30 22:45:04 +0300

    Update and rename stunipscan.py to stunipscanedit.py

--- stunipscanedit.py
+++ stunipscanedit.py
@@ -11,15 +11,7 @@ from lib.params import get_stunipscan_args
 def main():
     ip, host, rport, proto, verbose, user, pwd, ipdst = get_stunipscan_args()
 
-    s = StunIpscan()
-    s.ip = ip
-    s.host = host
-    s.rport = rport
-    s.proto = proto
-    s.verbose = verbose
-    s.user = user
-    s.pwd = pwd
-    s.ipdst = ipdst
+ 
 
     s.start()
 
